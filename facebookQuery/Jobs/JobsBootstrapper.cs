6524fcaf9a7dbfcf2f5af4ca42eb0d1c3461d7a8
DoctorSoft <megopixel91@gmail.com>
2016-12-03 16:39:24 +0300

    register jobs for all accounts

--- facebookQuery/Jobs/JobsBootstrapper.cs
+++ facebookQuery/Jobs/JobsBootstrapper.cs
@@ -12,16 +12,14 @@ namespace Jobs
         public static void SetUpJobs()
         {
             //todo: uncomment it back
-            //var account = new HomeService().GetAccounts().FirstOrDefault();
-
-            //RecurringJob.AddOrUpdate(string.Format("Refresh friends list for account = {0} )", account.Login), () => RefreshFriendsJob.Run(account.FacebookId), "* 0/1 * * *");
+            var accounts = new HomeService().GetAccounts();
+            foreach (var accountViewModel in accounts)
+            {
+                RecurringJob.AddOrUpdate(string.Format("Respond to unread messages from {0}", accountViewModel.Login), () => SendMessageToUnreadJob.Run(accountViewModel), Cron.Minutely);
+                RecurringJob.AddOrUpdate(string.Format("Respond to unanswered messages from {0}", accountViewModel.Login), () => SendMessageToUnansweredJob.Run(accountViewModel), Cron.Minutely);
+                RecurringJob.AddOrUpdate(string.Format("Refresh friends list for account = {0} )", accountViewModel.Login), () => RefreshFriendsJob.Run(accountViewModel.FacebookId), "* 0/1 * * *");
+            }
             //RecurringJob.AddOrUpdate(string.Format("Sending letters to new friends from = {0} )", account.Login), () => SendMessageToNewFriendsJob.Run(account), Cron.Minutely);
-
-            //RecurringJob.AddOrUpdate(string.Format("Receive unread messages for accountId = {0} )", account), () => GetUnreadMessagesJob.Run(account), "* 0/1 * * *");
-            
-            //var accounts = new HomeService().GetAccounts().Select(model => model.UserId).Take(2).ToList();
-            //RecurringJob.AddOrUpdate(string.Format("Respond to unread messages from {0}", account.Login), () => SendMessageToUnreadJob.Run(account), Cron.Minutely);
-            //RecurringJob.AddOrUpdate(string.Format("Respond to unanswered messages from {0}", account.Login), () => SendMessageToUnansweredJob.Run(account), Cron.Minutely);
         }
     }
 }
